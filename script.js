b49b560c8a67a072a27cd98d5974bbdc093aef73
24109jm <24109jm@hvhs.school.nz>
2026-07-24 22:47:02 +1200

    Bringing progress onto my device as it didn't save over

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ let NLMquantity = 0;
 let ZBquantity = 0;
 let OFquantity = 0;
 let RLBquantity = 0;
+let NLMprice = 2.36;
+let ZBprice = 9.44;
+let OFprice = 4.72;
+let RLBprice = 2.36;
+
+let messages = ["Have a good afternoon", "Enjoy your meal", "Bon Appetit", "Thank you for ordering from us", "We appreciate your support", "Have a great rest of your day", "Savor every bite!", "You made our day", "Come back soon!", "It's a pleasure serving you"]
 
 const NLMminus = document.getElementById("NLMminus");
 const NLMquant = document.getElementById("NLMquant");
 const NLMplus = document.getElementById("NLMplus");
 
-const ZBminus = document.getElementById("NLMminus");
-const ZBquant = document.getElementById("NLMquant");
-const ZBplus = document.getElementById("NLMplus");
+const ZBminus = document.getElementById("ZBminus");
+const ZBquant = document.getElementById("ZBquant");
+const ZBplus = document.getElementById("ZBplus");
 
-const OFminus = document.getElementById("NLMminus");
-const OFquant = document.getElementById("NLMquant");
-const OFplus = document.getElementById("NLMplus");
+const OFminus = document.getElementById("OFminus");
+const OFquant = document.getElementById("OFquant");
+const OFplus = document.getElementById("OFplus");
 
-const RLBminus = document.getElementById("NLMminus");
-const RLBquant = document.getElementById("NLMquant");
-const RLBplus = document.getElementById("NLMplus");
+const RLBminus = document.getElementById("RLBminus");
+const RLBquant = document.getElementById("RLBquant");
+const RLBplus = document.getElementById("RLBplus");
+
+const orderID = Math.floor(Math.random() * 1000000000).toString().slice(1);
+const messageNum = orderID[0];
 
 NLMminus.addEventListener("click", function() {
     if (NLMquantity > 0) {
@@ -67,20 +76,82 @@ RLBplus.addEventListener("click", function() {
     RLBquant.value = RLBquantity;
 });
 
-function fix(input) {
-    let value = Number(input.value);
+NLMquant.addEventListener("change", function() {
+    NLMquantity = Number(NLMquant.value);
 
-    if (value < 0 || isNaN(value)) {
-        value = 0;
-    }
-
-    input.value = value;
-    return vlaue;
-};
-
-NLMquant.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        NLMquantity = fix(NLMquant);
+    if (NLMquantity < 0) {
+        NLMquantity = 0;
+        NLMquant.value = 0;
     }
 });
 
+ZBquant.addEventListener("change", function() {
+    ZBquantity = Number(ZBquant.value);
+
+    if (ZBquantity < 0) {
+        ZBquantity = 0;
+        ZBquant.value = 0;
+    }
+});
+
+OFquant.addEventListener("change", function() {
+    OFquantity = Number(OFquant.value);
+
+    if (OFquantity < 0) {
+        OFquantity = 0;
+        OFquant.value = 0;
+    }
+});
+
+RLBquant.addEventListener("change", function() {
+    RLBquantity = Number(RLBquant.value);
+
+    if (RLBquantity < 0) {
+        RLBquantity = 0;
+        RLBquant.value = 0;
+    } 
+});
+
+function receipt() {
+    const NAME_FIELD = document.getElementById("nameField");
+    let name = NAME_FIELD.value;
+    const PAID_FIELD = document.getElementById("moneyField");
+    let paid = PAID_FIELD.value;
+    const OUTPUT = document.getElementById("orderScreen");
+    OUTPUT.innerHTML = "<h3>Receipt</h3><p>Order ID: #"+ orderID +"<p>Name: "+ name +"</p><p>Order Total: $"+ ((NLMprice * NLMquantity) + (ZBprice * ZBquantity) + (OFprice * OFquantity) + (RLBprice * RLBquantity)).toFixed(2) +"<p>Total Paid: $"+ paid +"</p><p>Change: $"+ (((((NLMprice * NLMquantity) + (ZBprice * ZBquantity) + (OFprice * OFquantity) + (RLBprice * RLBquantity)).toFixed(2)) - paid).toFixed(2)) * -1 +"</p><p>"+ messages[Number(messageNum)] +"</p>";
+}
+
+function buy() {
+    const OUTPUT = document.getElementById("orderScreen");
+    OUTPUT.innerHTML = "<h3>Please enter your details:</h3>";
+    OUTPUT.innerHTML += "<form onsubmit='return false;'><label for='formName'>Name: </label><input type='text' id='nameField' placeholder='Eg. Fred McKenzie'></br><label for='formMoney'>Paying: $</label><input type='number' id='moneyField' placeholder='Eg. 236'></br><input type ='submit' onClick=receipt()></br></form>";
+}
+
+function order() {
+    const OUTPUT = document.getElementById("orderScreen");
+    OUTPUT.innerHTML = "<h3>Ready to buy?</h3>"; 
+    if (NLMquantity === 1) {
+        OUTPUT.innerHTML += "<p>1x Natural Lime Milkshake ($"+ (NLMprice * NLMquantity).toFixed(2) +")</p>"; 
+    } else if (NLMquantity > 1) {
+        OUTPUT.innerHTML += "<p>"+ NLMquantity +"x Natural Lime Milkshakes ($"+ (NLMprice * NLMquantity).toFixed(2) +")</p>";
+    };
+    if (ZBquantity === 1) {
+        OUTPUT.innerHTML += "<p>1x Zero Burger ($"+ (ZBprice * ZBquantity).toFixed(2) +")</p>"; 
+    } else if (ZBquantity > 1) {
+        OUTPUT.innerHTML += "<p>"+ ZBquantity +"x Zero Burgers ($"+ (ZBprice * ZBquantity).toFixed(2) +")</p>";
+    };
+    if (OFquantity >= 1) {
+        OUTPUT.innerHTML += "<p>"+ OFquantity +"x Only Fries ($"+ (OFprice * OFquantity).toFixed(2) +")</p>"; 
+    };
+    if (RLBquantity === 1) {
+        OUTPUT.innerHTML += "<p>1x Real Life Brownie ($"+ (RLBprice * RLBquantity).toFixed(2) +")</p>"; 
+    } else if (RLBquantity > 1) {
+        OUTPUT.innerHTML += "<p>"+ RLBquantity +"x Real Life Brownies ($"+ (RLBprice * RLBquantity).toFixed(2) +")</p>";
+    };
+    if (NLMquantity === 0 && ZBquantity === 0 && OFquantity === 0 && RLBquantity === 0) {
+        OUTPUT.innerHTML += "<p>Please add to your order before attempting to purchase.</p>";
+    } else {
+        OUTPUT.innerHTML += "<p>Total: $"+ ((NLMprice * NLMquantity) + (ZBprice * ZBquantity) + (OFprice * OFquantity) + (RLBprice * RLBquantity)).toFixed(2) +"";
+        OUTPUT.innerHTML += '<button onClick="buy()">Buy</button>';
+    };
+};
